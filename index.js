acf2ca7db9622ce933a809fc4d2895b5682f9b6e
Loki Rautio <lokio27productions@gmail.com>
2019-02-04 13:49:37 -0600

    Prep towards using file structure

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+/*********************************************************
+* index.js - Main Wrapper
+**********************************************************
+* This file is for connecting everything to Discord via
+* discord.js. All modules will interface with code in
+* this file. 
+* No functionality will be defined in this file.
+*/
+
+const fs = require("fs");
+
+var modulePaths = [
+	/* priority with 0 being highest priority
+	["modules/core/commands.js"],
+	["modules/general/commands.js"]
+	*/
+]
+
+var moduleDirs = fs.readdirSync("modules/");
+for(var i = 0; i < moduleDirs.length; i++){
+	var modFiles = fs.readdirSync("modules/" + moduleDirs[i].toString() + "/")
+	// get metadata from module js exports
+	// set priorities
+}
+
+// load modules in correct order
+// register everything from modules
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
+
---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+title Kerr
+:start
+node index.js
+pause
+cls
+echo Bot is restarting...
+echo.
+goto start
