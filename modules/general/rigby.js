cf33afc2a5ac3382d21a13172424e7b23861ddc6
codeHusky <lokio27productions@gmail.com>
2019-02-05 23:39:08 -0600

    Rigby scraping

--- modules/general/rigby.js
+++ modules/general/rigby.js
@@ -11,3 +11,36 @@ module.exports = {
 		}
 	}
 };
+
+const rp = require('request-promise');
+const $ = require('cheerio');
+const url = 'https://rigby.space/browse/recent';
+rp({url:url,strictSSL: false})
+	.then(function(html){
+		//success!
+		var posts = $('.linkList', html);
+		for(var i = 0; i < posts.length; i++){
+			var post = $(posts[i]);
+			var link = $("a",post)[0].attribs.href;
+			rp({url:"https://rigby.space" +  link,strictSSL: false})
+				.then(function(html){
+					//success!
+					var id = $('#voteButton', html)[0].attribs.href.split("/");
+						id = id[id.length-1];
+					var content = $('pre', html).text();
+					var title = $("h3",html).text();
+					var author = $("#authorLink > a",html).text();
+					var aL = $("#authorLink", html).text().split(" ");
+					var postDate = aL[aL.length-1];
+				})
+				.catch(function(err){
+					//handle error
+					console.log(err);
+				});
+		}
+		//console.log($('.linkList', html));
+	})
+	.catch(function(err){
+		//handle error
+		console.log(err);
+	});
